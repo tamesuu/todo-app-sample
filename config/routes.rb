b5d117de00d957b5f609c91d32289c9d5356c087
tamesuu <aganoyot@gmail.com>
2021-05-10 08:13:21 +0900

    newを動くようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Label::Engine, at: '/label'
+  mount Task::Engine, at: '/task'
 end
